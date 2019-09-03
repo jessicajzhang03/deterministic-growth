d2af3c6b40e48505ac8e41d13788caa4cbe4ff06
Jessica Zhang <jessicajzhang03@gmail.com>
2019-09-03 13:20:02 -0700

    Points in X_n

--- growth.py
+++ growth.py
@@ -1,2 +1,23 @@
 import math 
+from collections import Counter
+import pprint as pp
 
+upgrade = set([(0,1),(1,0),(0,-1),(-1,0)])
+
+def points_up_to(n):
+    if not n:
+        return set([(0,0)])
+    prev = points_up_to(n-1)
+    c = Counter((i[0]+j[0],i[1]+j[1]) for i in prev for j in upgrade)
+    added = [x for x in c if c[x]==1]
+    return prev.union(added)
+
+# def points_first_in(n):
+#     if not n:
+#         return set([0,0])
+#     prev = points_up_to(n-1)
+#     c = Counter((i[0]+j[0],i[1]+j[1]) for i in prev for j in upgrade)
+#     added = [x for x in c if c[x]==1]
+#     return added
+
+pp.pprint(points_up_to(5))
